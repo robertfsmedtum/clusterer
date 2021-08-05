fbbb39134bff6126f4448922f9639821e90e11e6
robertfsmedtum <robertfsmedtum@gmail.com>
2021-08-05 09:57:19 +0200

    bugfix series datatype

--- clusterer.py
+++ clusterer.py
@@ -517,6 +517,7 @@ def main():
                         ndf[col] = pd.cut(ser, bins=bins, labels=labels)
                     else:
                         if 0 in different_values and 1 in different_values and len(different_values) == 2:
+                            ser = ser.astype('category')
                             ser = ser.replace(1, col)
                             ser = ser.replace(0, '')
                         else:
